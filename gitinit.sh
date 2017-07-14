987ffcac5dec8aef3ab35dcd2b90eb6f5fcb21b3
Thang Nguyen <thangnt.az@gmail.com>
2017-07-14 15:25:52 +0700

    add src BimaxCore as submodule, edit gitini.sh

--- gitinit.sh
+++ gitinit.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 git submodule update --init --recursive
 git submodule update --recursive --remote
+
+cd src && git submodule update --init --recursive
+cd src && git submodule update --recursive --remote
+
