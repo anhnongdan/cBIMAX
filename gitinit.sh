16c1f36b6225a16f101a0b810f72dc5921db900e
Thang Nguyen <thangnt.az@gmail.com>
2017-07-14 16:38:38 +0700

    fix gitinit

--- gitinit.sh
+++ gitinit.sh
@@ -2,6 +2,5 @@
 git submodule update --init --recursive
 git submodule update --recursive --remote
 
-cd src && git submodule update --init --recursive
-cd src && git submodule update --recursive --remote
+cd src && git submodule update --init --recursive && git submodule update --recursive --remote
 
